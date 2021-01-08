adebb671ff3466bb36752cd8cb3b0ff4a2c985ad
unknown <itaihammer1@gmail.com>
2021-01-07 18:56:01 -0800

    Added Heroku support

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import Discord from 'discord.js';
 import path from 'path';
 import fs from 'fs';
+import dotenv from 'dotenv';
 import fetch from 'node-fetch';
 
 import {
@@ -9,10 +10,14 @@ import {
     prefixCommand,
     prefixChange,
     setAdminRole,
-    adminRole
+    adminRole,
+    ping
 } from './commands.js';
 
-const token = 'Nzk2MjM5Njg3MDEwMDkxMDQ4.X_VB_g.1hfq0QZS48mGVhxCoY_GlinoDHo';
+const __dirname = path.resolve(path.dirname(''));
+dotenv.config();
+const token = process.env.TOKEN;
+const isHeroku = process.env.NODE_ENV === 'production';
 const client = new Discord.Client();
 
 client.on('guildCreate', (guild) => {
@@ -51,7 +56,8 @@ client.on('guildCreate', (guild) => {
     **-** Everyone who has Administrator permisions and or the admin role can use admin commands.
     **-** The current admin role is <@&${
         guild.roles.cache.find((role) => role.name === adminRole).id
-    }>, to change the admin role use the \`!setadminrole <Role Name>\` command.`
+    }>, to change the admin role use the \`!setadminrole <Role Name>\` command.
+    **-** For any additional help, support, or for any bug reports you can go to the bot's github page at: https://github.com/ItaiHammer/Tasty-Bot`
         )
         .setImage(
             'https://images-ext-1.discordapp.net/external/pOg4UB3nzzWxeXPz_2UMAZ9flnNtHtSIRhaSC1mFxxo/https/cdn.discordapp.com/avatars/357204752817192963/016616d966561ad13d5354acbc56e676.webp'
@@ -112,8 +118,10 @@ client.on('message', (msg) => {
         //*MSG VARS
         let query = msg.content.includes(' ')
             ? msg.content.substr(prefixLength).split(' ')
-            : msg.content;
-        let keyWord = msg.content.includes(' ') ? query[0] : msg.content;
+            : msg.content.substr(prefixLength);
+        let keyWord = msg.content.includes(' ')
+            ? query[0]
+            : msg.content.substr(prefixLength, msg.content.length);
 
         //if it has a mention
         if (msg.content.substr(0, botMention.length) === botMention) {
@@ -138,6 +146,9 @@ client.on('message', (msg) => {
             case 'adminrole':
                 adminRole(msg);
                 break;
+            case 'ping':
+                ping(msg);
+                break;
         }
 
         //if member has the admin role
@@ -148,11 +159,6 @@ client.on('message', (msg) => {
             !msg.member.permissions.has('ADMINISTRATOR')
         )
             return;
-
-        console.log(msg.content.substr(prefixLength).split(' '));
-        console.log(prefixLength);
-        console.log(query[0]);
-        console.log(keyWord);
 
         switch (keyWord) {
             case 'channelhelp':
@@ -169,6 +175,13 @@ client.on('message', (msg) => {
             console.log(error);
             // msg.reply(`an error has accured, make sure you typed the command correctly. If you still need any more help you can use the ${prefix}help command.`);
         }
+    }
+
+    if (isHeroku === true) {
+        setInterval(async () => {
+            await fetch('/');
+            console.log('fetched');
+        }, 60000);
     }
 });
 
