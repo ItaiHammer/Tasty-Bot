7160e3b9720291f2503a868e8c8f833d0b978213
Michael Slain <misha.slain@icloud.com>
2021-01-07 19:34:01 -0800

    Add support for heroku port

---
+++ server.js
@@ -0,0 +1,8 @@
+import express from 'express'
+import './index.js'
+
+const port = process.env.PORT || 5000
+
+express()
+.get('/', (req, res) => res.send('welcome!'))
+.listen(port, () => console.log(`Server is running on ${port}`)
