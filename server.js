ea61bdba2538d5a6b59522272c63c45e42ae85ce
unknown <itaihammer1@gmail.com>
2021-02-02 13:27:04 -0800

    Added server reset fix 2

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
 import express from 'express';
+import path from 'path';
 import fs from 'fs';
 import './index.js';
 
 const port = process.env.PORT || 5000;
+const __dirname = path.resolve(path.dirname(''));
 
 express()
     .get('/', (req, res) => res.send('welcome!'))
     .get('/server/data', (req, res) => {
-        const serverData = JSON.parse(
-            fs.readFileSync('./serverproperties.json')
-        );
-        res.sendFile(serverData);
+        res.sendFile(path.join(__dirname, 'serverproperties.json'));
     })
     .listen(port, () => console.log(`Server is running on ${port}`));
 
